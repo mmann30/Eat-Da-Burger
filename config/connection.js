153f866ca19ac525bf9556fdbcb14004d9a88b7e
mmann30 <michael.l.mann13@gmail.comm>
2017-08-14 19:19:47 -0400

    removed port

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     var connection = mysql.createConnection({
-    port: 3306,
     host: "localhost",
     user: "root",
     password: "", //need to provide local password
