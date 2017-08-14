849cf1a77704a2bce17090d6413305beead92517
mmann30 <michael.l.mann13@gmail.comm>
2017-08-14 19:15:19 -0400

    add JawsDB connection for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,30 @@
 // Node to MySQL database connection
 var mysql = require('mysql');
-
-var connection = mysql.createConnection({
+var connection;
+//////////////////////////////////////////
+// JawsDB/Heroku Connection
+//////////////////////////////////////////
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "",
+    password: "", //need to provide local password
     database: "burgers_db"
-});
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
         console.error('error connection: ' + err.stack);
         return;
     }
-
     console.log('connected as id ' + connection.threadId);
 });
 
 // Export for use in ORM.js
 module.exports = connection;
+
+
